2bf09a553339f7445a7fb8774490cde64c58d40b
Natanael Rabello <natanael.rabello@outlook.com>
2019-01-21 21:27:52 -0200

    Add initial proxies structure
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- src/main/java/natanaeljr/kickoff/KickOffMod.java
+++ src/main/java/natanaeljr/kickoff/KickOffMod.java
@@ -1,8 +1,9 @@
 package natanaeljr.kickoff;
 
-import net.minecraft.init.Blocks;
+import natanaeljr.kickoff.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -12,28 +13,42 @@ import org.apache.logging.log4j.Logger;
      acceptedMinecraftVersions = "1.12.2", acceptableSaveVersions = "1.12.2")
 public class KickOffMod
 {
+    /* Constants */
     public static final String MODID = "kickoff";
     public static final String NAME = "Kick-Off";
     public static final String VERSION = "0.1-alpha";
 
-    private static Logger logger;
+    /* Instance */
+    @Mod.Instance
+    public static KickOffMod INSTANCE;
 
+    /* Static fields */
+    public static Logger logger;
+
+    @SidedProxy(clientSide = "natanaeljr.kickoff.proxy.ClientProxy",
+                serverSide = "natanaeljr.kickoff.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    /* Event Handlers */
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
-        logger.info(NAME + " pre-initialization phase");
+        logger.info("pre-initialization phase");
+        proxy.preInit(event);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        logger.info(NAME + " initialization phase");
+        logger.info("initialization phase");
+        proxy.init(event);
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-        logger.info(NAME + " post-initialization phase");
+        logger.info("post-initialization phase");
+        proxy.postInit(event);
     }
 }
